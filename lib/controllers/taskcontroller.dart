2df718de187da8204ed6b3ef5de4c3104c2e79df
Hania Arshad <haniaarshad890@gmail.com>
2026-02-05 23:35:48 +0500

    Task app

---
+++ lib/controllers/taskcontroller.dart
@@ -0,0 +1,13 @@
+import 'package:notes_app/models/taskmodel.dart';
+
+class TaskListController {
+  List<Task> tasks = [
+    Task('Task 1', false),
+    Task('Task 2', true),
+    Task('Task 3', false),
+  ];
+
+  void toggleTaskCompletion(int index) {
+    tasks[index].completed = !tasks[index].completed;
+  }
+}
